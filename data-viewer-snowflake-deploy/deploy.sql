7147e81851c7f0a16dc475a93f3b845b1f4e55fe
Carl Neal <CNeal@inspirebrands.com>
2024-07-16 13:57:57 -0400

    Updating Local and Snowflake Deploy Applications

---
+++ data-viewer-snowflake-deploy/deploy.sql
@@ -0,0 +1,19 @@
+-- to be deployed as a Streamlit App with: snowsql -c demo_conn -f deploy.sql
+!set variable_substitution=true
+!define CRT_DIR=file://C:\Users\Work\Documents\phData\streamlit\streamlit-snowflake-demo\data-viewer-snowflake-deploy
+
+use schema streamlit_demo.public;
+
+create stage if not exists streamlit_stage;
+
+put &CRT_DIR\app.py @streamlit_stage overwrite=true auto_compress=false;
+put &CRT_DIR\modules\*.py @streamlit_stage/modules overwrite=true auto_compress=false;
+put &CRT_DIR\data\*.csv @streamlit_stage/data overwrite=true auto_compress=false;
+-- put &CRT_DIR\animated\templates\*.html @streamlit_stage/animated/templates overwrite=true auto_compress=false;
+put &CRT_DIR\environment.yml @streamlit_stage overwrite=true auto_compress=false;
+
+create or replace streamlit data_viewer_snowflake
+    root_location = '@streamlit_demo.public.streamlit_stage'
+    main_file = '/app.py'
+    query_warehouse = 'STREAMLITWAREHOUSE';
+show streamlits;
